43ecb9f448b01900f92688f59fdbc9676128081a
Brian Hasal <brian.hasal@gmail.com>
2022-07-21 06:17:35 -0700

    14a

--- 14 - converting_data_practice_problems.rb
+++ 14 - converting_data_practice_problems.rb
@@ -1,8 +1,19 @@
 #  1. Convert an array of arrays into a hash.
 #     For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
 
+array = [[1, 3], [8, 9], [2, 16]]
 
-# p out_hash
+
+def array_hasher(array)
+
+  out_hash = Hash.new
+  array.each do |key, value|
+    out_hash[key] = value
+  end
+  return out_hash
+end
+
+p array_hasher(array)
 
 
 #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
