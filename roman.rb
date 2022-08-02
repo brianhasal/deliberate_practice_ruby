4920ce892e1b7f9686634610d3f6cc6e775cb096
Brian Hasal <brian.hasal@gmail.com>
2022-08-02 06:33:13 -0700

    Used modulo to separate each denomination into four variables

--- roman.rb
+++ roman.rb
@@ -50,3 +50,31 @@
 # 1 <= s.length <= 15
 # s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 # It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+
+
+int = 3798
+
+ones = int % 10
+p ones
+
+int -= ones
+
+p int
+
+tens = int % 100
+
+p tens
+
+int -= tens
+
+p int
+
+
+hundreds = int % 1000
+
+p hundreds
+
+thousands = int - hundreds
+
+p thousands
