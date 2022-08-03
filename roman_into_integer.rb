b5a550ec4320071ae5038ab3e4c89268c0e7c03b
Brian Hasal <brian.hasal@gmail.com>
2022-08-03 05:46:16 -0700

    Roman Numerals to Integer finished #1

--- roman_into_integer.rb
+++ roman_into_integer.rb
@@ -11,6 +11,7 @@
 #    400  C     D           C  500
 #    900  C     M           C 1000
 
+
 # For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
 # Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
@@ -24,7 +25,7 @@
 
 # Example 1:
 
-# Input: s = "III"
+string1 = "III"
 # Output: 3
 # Explanation: III = 3.
 
@@ -32,7 +33,7 @@
 
 # Example 2:
 
-# Input: s = "LVIII"
+string2 = "LVIII"
 # Output: 58
 # Explanation: L = 50, V= 5, III = 3.
 
@@ -40,7 +41,7 @@
 
 # Example 3:
 
-# Input: s = "MCMXCIV"
+string3 = "MCMXCIV"
 # Output: 1994
 # Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
  
@@ -51,12 +52,58 @@
 # s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 # It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 
+
+
 def roman_to_int(string)
-  
-  if 'IV'
 
-  elsif 'IX'
+  total = 0
 
+  if string.include?("CM")
+    string.sub!('CM', '')
+    total += 900
   end
 
+  if string.include?("CD")
+    string.sub!('CD', '')
+    total += 400
+  end
+
+  if string.include?("XC")
+    string.sub!('XC', '')
+    total += 90
+  end
+
+  if string.include?("XL")
+    string.sub!('XL', '')
+    total += 40
+  end
+
+  if string.include?("IX")
+    string.sub!('IX', '')
+    total += 9
+  end
+
+  if string.include?("IV")
+    string.sub!('IV', '')
+    total += 4
+  end
+  
+  total += string.count("M") * 1000
+  total += string.count("D") * 500
+  total += string.count("C") * 100
+  total += string.count("L") * 50
+  total += string.count("X") * 10
+  total += string.count("V") * 5
+  total += string.count("I")
+  
+  return total  
+
 end
+
+
+p roman_to_int(string1)
+p roman_to_int(string2)
+p roman_to_int(string3)
+p roman_to_int("MMMCMXCIX")
+p roman_to_int("MMMDCCCLXXXVIII")
+
