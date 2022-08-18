b132fb3e04cb65b23e62e17b62c00db8b6c4946f
Brian Hasal <brian.hasal@gmail.com>
2022-08-18 05:59:37 -0700

    Setup

--- F - data_transformations.rb
+++ F - data_transformations.rb
@@ -56,10 +56,13 @@ p hash_flatten(input)
 # Description
 # Given a hash, create a new hash that has the keys and values switched.
 
-# Input: {"a" => 1, "b" => 2, "c" => 3}
+input = {"a" => 1, "b" => 2, "c" => 3}
 # Output: {1 => "a", 2 => "b", 3 => "c"}
 
+def flipper(hash)
+  
 
+end
 
 
 
